1f352fec0ad8db64bfba1b5519039566d974f77b
161250167 <1158873864@qq.com>
2019-01-28 20:33:08 +0800

    配置

--- JRQ.Frontend/app.js
+++ JRQ.Frontend/app.js
@@ -141,9 +141,9 @@ App({
     wechatUsername: "",
     token: "",
     defaultPic: '../../default/default-pic.png',
-    // backendUrl: "https://junrongcenter.com:3389/",//used
+     backendUrl: "https://junrongcenter.com:3389/",//used
     picUrl: "https://www.junrongcenter.com/",//used
-    backendUrl: "http://localhost:8080/",
+    //backendUrl: "http://localhost:8080/",
     //picUrl: "http://localhost:8000/",
     //picUrl: "http://localhost/libs/"//xulei
   }

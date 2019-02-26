1a86f4310e4f059b3c55cfe7ece531ff32d27a8d
Manecho <2064627646@qq.com>
2019-02-26 17:14:15 +0800

    修改我的项目

--- JRQ.Frontend/pages/me/userInfo.js
+++ JRQ.Frontend/pages/me/userInfo.js
@@ -1,4 +1,7 @@
 // pages/me/userInfo.js
+const app = getApp()
+var api = require('../../util/api.js')
+import articleItem from '../../template/article/articleItem'
 Page({
 
   /**
@@ -9,7 +12,7 @@ Page({
     lastId: "",
     lastIdType: "",
 
-    myInfo: {
+    userInfo: {
       // writerface:'../../default/default-icon.png',
       axis: [
         {
@@ -56,15 +59,207 @@ Page({
       company: '北京大同信托有限公司',
       
       label: ['内构重组', '短融过桥', '大宗交易', '银行业务', '包里融租']
+    },
+    cardLimits: 0,
+    myInfo: {
+      username: 'USERNAME',
+      medals: [
+        '../../../default/default-icon.png',
+        '../../../default/default-icon.png',
+        '../../../default/default-icon.png',
+        '../../../default/default-icon.png'],
+      phone: '18512346956',
+      email: '123456789@163.com',
+      company: '美国永辉有限公司',
+      department: 'IT技术部',
+      position: 'IT初级经理',
+      intro: '我要在代码的世界里飞翔。'
+    },
+    isMyInfoVisiable: false,
+    isGetOtherInfo: null,
+    isAlreadyGetOtherInfo: null,
+    otherid: null,
+    encreptInfo: {
+      phone: '************',
+      wechatId: '****',
+      email: '******'
+    },
+    myArticles: [
+      {
+        id: 1,
+        text: '《有效识别金融项目》课程。',
+        images: [
+          '../../../default/default-pic.png',
+          '../../../default/default-pic.png',
+          '../../../default/default-pic.png'
+        ],
+        writerFace: '../../../default/default-icon.png',
+        writerName: 'USERNAME',
+        date: '2020-01-01',
+        likeNum: 8965
+      },
+      {
+        id: 2,
+        text: '《有效识别金融项目》课程。',
+        images: [
+          '../../../default/default-pic.png',
+          '../../../default/default-pic.png',
+          '../../../default/default-pic.png'
+        ],
+        writerFace: '../../../default/default-icon.png',
+        writerName: 'USERNAME',
+        date: '2020-01-01',
+        likeNum: 8965
+      },
+      {
+        id: 3,
+        text: '《有效识别金融项目》课程。',
+        images: [
+          '../../../default/default-pic.png',
+          '../../../default/default-pic.png',
+          '../../../default/default-pic.png'
+        ],
+        writerFace: '../../../default/default-icon.png',
+        writerName: 'USERNAME',
+        date: '2020-01-01',
+        likeNum: 8965
+      }
+    ]
+  },
+
+  /**
+     * 生命周期函数--监听页面加载
+     */
+  onLoad: function (options) {
+    if (options.id) {
+      this.data.isGetOtherInfo = true
+      this.data.isAlreadyGetOtherInfo = false
+      this.data.otherid = options.id
+      api.getOtherBasicInfo.call(this, this.data.otherid) //获取除联系方式外的其他信息
+      api.getUserHistoryAbstractList.call(this, app.getOpenid(), this.data.otherid) //获取文章历史记录
     }
+    else {
+      var that = this
+      this.data.isGetOtherInfo = false
+      api.getMyInfo.call(this, app.getOpenid()) //获取个人信息
+      api.getUserHistoryAbstractList.call(this, app.getOpenid(), app.getOpenid()) //获取个人历史文章列表信息
+    }
+    api.getMyCardLimits.call(this, app.getOpenid())
+  },
+
+  //点击查看联系方式
+  isMyInfoVisiableToggle: function () {
+    var that = this
+    if (this.data.isGetOtherInfo) {        //获取别的用户信息
+      if (!this.data.isAlreadyGetOtherInfo) {        //还没有获取当前用户信息
+        //向服务器发送请求询问是否已有权限获取详细信息，如果已有权限则直接获取
+        api.isOtherCardAccessible.call(this, app.getOpenid(), this.data.otherid, (res) => {
+          if (res) {
+            //服务器返回可成功获取详细信息的权限，直接获取详细信息
+            api.getOtherInfo.call(that, app.getOpenid(), that.data.otherid, () => {
+              that.setData({
+                isMyInfoVisiable: !that.data.isMyInfoVisiable,
+              })
+              api.getMyCardLimits.call(that, app.getOpenid())
+              that.data.isAlreadyGetOtherInfo = true
+            })
+          } else {
+            //服务器返回没有权限获取详细信息，小程序向用户发起询问
+            if (that.data.cardLimits > 0) {    //用户查看次数足够
+              wx.showModal({
+                title: '是否确认查看用户信息?',
+                content: '您剩余查看次数为：' + that.data.cardLimits + '次',
+                success: (res) => {
+                  if (res.confirm) {
+                    //向服务器发送请求查看当前用户信息
+                    api.getOtherInfo.call(that, app.getOpenid(), that.data.otherid, () => {       //服务器返回成功
+                      that.setData({
+                        isMyInfoVisiable: !that.data.isMyInfoVisiable,
+                      })
+                      api.getMyCardLimits.call(that, app.getOpenid())
+                      that.data.isAlreadyGetOtherInfo = true
+                    })
+                  }
+                }
+              })
+            }
+            else {                             //用户查看次数不足
+              wx.showModal({
+                title: '今日查看次数不足',
+                content: '是否消耗5个钧融币查看？',
+                success: (res) => {
+                  if (res.confirm) {
+                    //向服务器发送请求查看当前用户信息
+                    api.getOtherInfo.call(that, app.getOpenid(), that.data.otherid, () => {       //服务器返回成功
+                      that.setData({
+                        isMyInfoVisiable: !that.data.isMyInfoVisiable,
+                      })
+                      api.getMyCardLimits.call(that, app.getOpenid())
+                      that.data.isAlreadyGetOtherInfo = true
+                    }, () => {                         //服务器返回失败
+                      wx.showModal({
+                        title: '获取用户信息失败',
+                        content: '查看次数和金额都不足',
+                        showCancel: false
+                      })
+                    })
+                  }
+                }
+              })
+            }
+          }
+        })
+      }
+      else {                              //已经获取当前用户信息
+        that.setData({
+          isMyInfoVisiable: !that.data.isMyInfoVisiable,
+        })
+      }
+
+    }
+    else {                               //获取自己信息
+      api.getMyInfo.call(this, app.getOpenid(), () => {
+        that.setData({
+          isMyInfoVisiable: !that.data.isMyInfoVisiable,
+        })
+      })
+    }
+  },
+
+  onSendMyCard: function (e) {
+    /*console.log(e)*/
+    if (this.data.isGetOtherInfo) {
+      api.sendMyCard.call(this, app.getOpenid(), this.data.otherid, null, e.detail.formId, null, null, (res) => {
+        /*console.log(res)*/
+        wx.hideLoading()
+        wx.showToast({
+          icon: "none",
+          title: res.message,
+        })
+      })
+    }
+    else {
+      wx.showModal({
+        content: '无需给自己发名片',
+        showCancel: false
+      })
+    }
+  },
+
+  onBackToIndex: function () {
+    wx.switchTab({
+      url: '/pages/index/index',
+    })
+  },
+
+  previewImg: function (e) {
+    articleItem.previewImg(e)
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
 
-  },
 
   //发布信息
   onPublish: function () {
@@ -72,66 +267,5 @@ Page({
     wx.navigateTo({
       url: 'publishMyArticle/publishMyArticle',
     })
-  },
-
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-
-  // 修改
-  // 获取项目列表
-  getProjectList: function () {
-    this.setData({
-      articles: [],
-      lastId: "",
-      lastIdType: ""
-    })
-    api.getAbstractList.call(this, app.getOpenid(), this.data.projectId, this.data.projectIdType)
-  },
+  }
 })
---
+++ JRQ.Frontend/template/article/articleItem.js
@@ -0,0 +1,21 @@
+var articleItem = {
+  //图片点击事件
+  previewImg: function (event) {
+    var src = event.currentTarget.dataset.src;//获取data-src
+    var imgList = event.currentTarget.dataset.list;//获取data-list
+    //图片预览
+    wx.previewImage({
+      current: src, // 当前显示图片的http链接
+      urls: imgList // 需要预览的图片http链接列表
+    })
+  },
+
+  onClickThisFace: function (event) {
+    var id = event.currentTarget.dataset.id
+    wx.navigateTo({
+      url: '../me/myHistory/myHistory?id=' + id,
+    })
+  }
+}
+
+export default articleItem;

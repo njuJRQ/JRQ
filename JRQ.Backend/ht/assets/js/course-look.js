5023b54418d0b718e7a6783bfbb035b64654abdd
296894139 <296894139@qq.com>
2019-04-20 12:00:36 +0800

    添加details

--- JRQ.Backend/ht/assets/js/course-look.js
+++ JRQ.Backend/ht/assets/js/course-look.js
@@ -13,12 +13,20 @@ $.ajax(
         success: function (data) {
             document.getElementById("id").innerText=data.course.id;
             document.getElementById("title").innerText=data.course.title;
+            document.getElementById("details").innerText=data.course.detail;
             document.getElementById("image").src="../"+data.course.image;
             document.getElementById("writerName").innerText=data.course.writerName;
             document.getElementById("date").innerText=data.course.date;
             document.getElementById("likeNum").innerText=data.course.likeNum;
             document.getElementById("price").innerText=data.course.price;
+            text=" <div class=\"am-u-sm-4 am-u-md-2 am-text-right\">课程视频</div>"
+            data.course.videos=["record/course/video/f7918f679143489eaad677e99904882d.mp4","record/course/video/f7918f679143489eaad677e99904882d.mp4","record/course/video/f7918f679143489eaad677e99904882d.mp4"]
+            for(i in data.course.videos){
+                text=text+"<div class=\"am-u-sm-8 am-u-md-10\"><button type=\"button\" onclick=\"look('"+data.course.videos[i]+"')\">点击查看课程视频</button></div>"
+            }
+            document.getElementById("videos").innerHTML=text
             video=data.course.video;
+
         },
         error: function (xhr) {
             alert('动态页有问题噶！\n\n' + xhr.responseText);
@@ -27,7 +35,9 @@ $.ajax(
     }
 )
 
-function look(){
-    storage["thisVideo"]=video;
+function look(video1){
+    alert(video1)
+    storage["thisVideo"]=video1;
     window.location.href="tv.html";
+
 }

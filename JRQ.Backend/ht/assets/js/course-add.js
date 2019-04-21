5ee83af99838c9fdf1d9d19dede102ad0246a47c
296894139 <296894139@qq.com>
2019-04-21 12:42:06 +0800

    修改组合课程

--- JRQ.Backend/ht/assets/js/course-add.js
+++ JRQ.Backend/ht/assets/js/course-add.js
@@ -1,4 +1,5 @@
 $("#loader").hide();
+var index=1;
 function checkRate(input) {
     var re = /^[0-9]+.?[0-9]*$/; //判断字符串是否为数字 //判断正整数 /^[1-9]+[0-9]*]*$/
     var nubmer = document.getElementById(input).value;
@@ -10,7 +11,75 @@ function checkRate(input) {
     }
     return true;
 }
+function addFile(){
+    var fragment=document.createDocumentFragment();
+    var divNode=document.getElementById("container");
+    var newDiv=document.createElement("div");
+    newDiv.setAttribute("id",index.toString()+"delfile");
+    fragment.appendChild(newDiv);
+    var newInput=document.createElement("input");
+    newInput.setAttribute("type","file");
+    newInput.setAttribute("id",index.toString());
+    newInput.setAttribute("name","选择文件");
+    newDiv.appendChild(newInput);
+
+
+    var newInput=document.createElement("input");
+    newInput.setAttribute("type","button");
+    newInput.setAttribute("value","删除");
+    newInput.setAttribute("id",index.toString()+"del");
+    newInput.setAttribute("onclick","delFile()");
+    newDiv.appendChild(newInput);
+    index=index+1
+    divNode.appendChild(fragment);
+}
+function delFile(){
+    alert(event.target.id);
+    var divNode=document.getElementById("container");
+    var child=document.getElementById(event.target.id+"file");
+    divNode.removeChild(child);
+}
+
 function adduser() {
+
+    var videos=[]
+    var url=getUrl();
+    var i=0;
+    while(i<index){
+
+        var form2 = new FormData();
+        var str_i=i.toString()
+
+        form2.append('video',$('#'+str_i)[0].files[0]);
+
+        $.ajax({
+            url: url + "/courseVideo",
+            type: "POST",
+            data: form2,
+            enctype: 'multipart/form-data',
+            processData: false,
+            contentType: false,
+            cache: false,
+            async: false,
+            success: function (data) {
+                $("#loader").hide();
+                if(data!="") {
+
+                    videos.push(data);
+
+                }
+            },
+            error: function (xhr) {
+                $("#loader").hide();
+            }
+        });
+
+        //添加文件
+        i=i+1;
+
+    }
+
+
     if(checkRate("price")) {
         $("#loader").show();
         var url = getUrl();
@@ -26,14 +95,7 @@ function adduser() {
             return;
         }
         formData.append('image', el.files[0]);
-        // 上传视频
-        var video="";
-        var els = $('#video')[0];
-        var form = new FormData();
-        if (!els.files[0]) {
-            return;
-        }
-        form.append('video', els.files[0]);
+
         $.ajax({
             url: url + "/courseImage",
             type: "POST",
@@ -42,51 +104,40 @@ function adduser() {
             processData: false,
             contentType: false,
             cache: false,
+            async: false,
             success: function (data) {
+                alert(videos)
                 if(data!="") {
                     image = data;
                 }
-                $.ajax({
-                    url: url + "/courseVideo",
-                    type: "POST",
-                    data: form,
-                    enctype: 'multipart/form-data',
-                    processData: false,
-                    contentType: false,
-                    cache: false,
-                    success: function (data) {
-                        $("#loader").hide();
-                        if(data!="") {
-                            video = data;
-                        }
-                        $.ajax(
-                            {
-                                url: url + "/addCourse",
-                                data: {
-                                    title: $("#title").val(),
-                                    writerName: id,
-                                    date: date,
-                                    price: parseInt($("#price").val()),
-                                    image:image,
-                                    video:video,
-                                    isTextualResearchCourse:false
-                                },
-                                async: false,
-                                success: function (data) {
-                                    alert("添加成功");
-                                    window.location.href = "course.html";
-                                },
-                                error: function (xhr) {
-                                    //alert('动态页有问题噶！\n\n' + xhr.responseText);
-                                },
-                                traditional: true,
-                            }
-                        )
-                    },
-                    error: function (xhr) {
-                        $("#loader").hide();
+                $.ajax(
+                    {
+                        url: url + "/addCourse",
+                        data: {
+                            title: $("#title").val(),
+                            detail:$("#details").val(),
+                            writerName: id,
+                            date: date,
+                            price: parseInt($("#price").val()),
+                            image:image,
+                            videos:videos,
+                            isTextualResearchCourse:false
+                        },
+                        async: false,
+                        success: function (data) {
+                            alert("添加成功");
+                            window.location.href = "course.html";
+                        },
+                        error: function (xhr) {
+                            //alert('动态页有问题噶！\n\n' + xhr.responseText);
+                        },
+                        traditional: true,
                     }
-                });
+                )
+
+
+
+
             },
             error: function (xhr) {
                 $("#loader").hide();

2015abc335c69e70bf4ff418fef4d6e63dcb16bd
296894139 <296894139@qq.com>
2019-04-21 12:59:52 +0800

    course_look

--- JRQ.Backend/ht/assets/js/course-look.js
+++ JRQ.Backend/ht/assets/js/course-look.js
@@ -20,13 +20,12 @@ $.ajax(
             document.getElementById("likeNum").innerText=data.course.likeNum;
             document.getElementById("price").innerText=data.course.price;
             text=" <div class=\"am-u-sm-4 am-u-md-2 am-text-right\">课程视频</div>"
-            data.course.videos=["record/course/video/f7918f679143489eaad677e99904882d.mp4","record/course/video/f7918f679143489eaad677e99904882d.mp4","record/course/video/f7918f679143489eaad677e99904882d.mp4"]
+            //data.course.videos=["record/course/video/f7918f679143489eaad677e99904882d.mp4","record/course/video/f7918f679143489eaad677e99904882d.mp4","record/course/video/f7918f679143489eaad677e99904882d.mp4"]
             for(i in data.course.videos){
                 text=text+"<div class=\"am-u-sm-8 am-u-md-10\"><button type=\"button\" onclick=\"look('"+data.course.videos[i]+"')\">点击查看课程视频</button></div>"
             }
             document.getElementById("videos").innerHTML=text
             video=data.course.video;
-
         },
         error: function (xhr) {
             alert('动态页有问题噶！\n\n' + xhr.responseText);

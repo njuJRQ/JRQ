4a5fad0bf6ffecf9cbd2809e15dcdcc2cd726d83
SZL <1710342622@qq.com>
2019-04-29 18:16:01 +0800

    resume page finish

--- JRQ.Frontend/pages/particulars/jobwanted/jobwanted.js
+++ JRQ.Frontend/pages/particulars/jobwanted/jobwanted.js
@@ -39,6 +39,14 @@ Page({
    */
   onLoad: function(options) {
     this.loadPositionsByCity(this.data.objectArray[this.data.index].name);
+
+    /* test!!!!! 
+    wx.navigateTo({
+      url: '../resume/resume'
+    })
+    */
+
+
   },
   bindPickerChange: function(e) {
     this.setData({
@@ -66,9 +74,79 @@ Page({
         that.setData({
           jobCardItems: res.data.jobCardItems
         })
+
+
+
+        /* test!!!!
+        that.data.jobCardItems=[
+          {
+            advantage: "正式编制，无业务考核",
+            city: "苏州",
+            degree: "本科",
+            enterprise: "证券公司",
+            expectPosition: "证券柜员",
+            expectWage: "4500+",
+            id: "2c90ba4b69f87cdf0169f881fcb40000",
+            image: "https://www.junrongcenter.com/undefined",
+            introduction: "某证券公司柜员",
+            isFresh: false
+          },
+          {
+            advantage: "正式编制，无业务考核",
+            city: "苏州",
+            degree: "本科",
+            enterprise: "证券公司",
+            expectPosition: "文书顾问",
+            expectWage: "10000+",
+            id: "2c90ba4b69f87cdf0169f881fcb40001",
+            image: "https://www.junrongcenter.com/undefined",
+            introduction: "某证券公司柜员",
+            isFresh: false
+          },
+          {
+            advantage: "正式编制，无业务考核",
+            city: "苏州",
+            degree: "本科",
+            enterprise: "证券公司",
+            expectPosition: "总经理",
+            expectWage: "25000+",
+            id: "2c90ba4b69f87cdf0169f881fcb40002",
+            image: "https://www.junrongcenter.com/undefined",
+            introduction: "某证券公司柜员",
+            isFresh: false
+          }
+        ];
+        that.setData(that.data); */
       }
+      
     })
   },
+
+  checkResume: function(event){
+    //console.log(this.data.jobCardItems);
+    //console.log(event.currentTarget.dataset);
+    //expectPosition,city
+    //console.log(app.getOpenid());
+    var dataset = event.currentTarget.dataset;
+
+    var that = this;
+    api.getMyInfo.call(this, app.getOpenid(), () => {
+      
+      /* 复制myInfo到newMyInfo中 */
+      that.data.newMyInfo = that.data.myInfo    
+      that.setData(that.data)
+
+      var info = "?uid=" + app.getOpenid() + "&jid=" + dataset.jid + "&expectPosition=" + dataset.expectposition + "&city=" + dataset.city;
+
+      wx.navigateTo({
+        url: '../resume/resume' + info
+      })
+
+    })
+
+    
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
--- JRQ.Frontend/pages/particulars/resume/resume.js
+++ JRQ.Frontend/pages/particulars/resume/resume.js
@@ -7,35 +7,346 @@ Page({
    * 页面的初始数据
    */
   data: {
-    jobCardItem : {
-      name: '胡某某',
-      studu: '南京林业大学-平面设计',
-      image: "/img/user.png",
-      icon_user:'/img/icon_user.png',
-      icon_age:'/img/icon_age.png',
-      icon_eduo:"/img/icon_eduo.png",
-      experience:'两年实习经历',
-      expect:'求职期望',
-      type:'平面设计师,南京',
-      money:'10k-14k',
-      txt:"行业不限",
-      practice:'实习经历',
-      tilte_name:"南京某某某公司",
-      time:'2018.5.16-2019.9.26',
+    isPreviewing: false,   //是否处于预览模式
+
+    icon_user: '/img/icon_user.png',
+    icon_age: '/img/icon_age.png',
+    icon_eduo: "/img/icon_eduo.png",
+
+    resume : {
+      name: '胡某某', /* 姓名 */
+      face: "/img/user.png", /* 头像 */
+
+      isFresh: true,  /* 是否为应届生 */
+      degree: '本科',    /* 学历 */
+      age: 21,  /* 年龄 */
+      phone: '17321232126',
+      email: '12345_54321_123@qq.com',
+      experience: 2,  /* 实习经历（年） */
       
-    }
+      expectPosition: '平面设计师',  /* 期待岗位 */
+      expectCity: '南京', /* 期待工作地点 */
+      lowWage: 10,  /* 期待最低月薪 */
+      highWage: 14, /* 期待最高月薪 */
+
+      internship: [{
+          company: "南京某某某公司",
+          fromTime: '2018.05.16',
+          toTime: '2019.09.26',
+          position: '平面设计师',
+          grade: '表现良好',
+          content: '负责1981现代林园的招标广告设计及制作，同时兼任团队美术顾问及总监'
+        },
+        {
+          company: "南京可嘴可乐有限公司",
+          fromTime: '2017.05.16',
+          toTime: '2017.09.26',
+          position: '艺术总监',
+          grade: '表现优异',
+          content: '负责可嘴可乐瓶装汽水的外形设计，标签设计，商标设计及广告设计负责可嘴可乐瓶装汽水的外形设计，标签设计，商标设计及广告设计负责可嘴可乐瓶装汽水的外形设计，标签设计，商标设计及广告设计负责可嘴可乐瓶装汽水的外形设计，标签设计，商标设计及广告设计负责可嘴可乐瓶装汽水的外形设计，标签设计，商标设计及广告设计负责可嘴可乐瓶装汽水的外形设计，标签设计，商标设计及广告设计'
+        }
+      ],
+
+      education: [{
+          school: "南京林业大学",
+          fromTime: '2012',
+          toTime: '2016',
+          degree: '本科',
+          major: '平面设计',
+        },
+        {
+          school: "南京林业大学",
+          fromTime: '2016',
+          toTime: '2020',
+          degree: '硕士',
+          major: '平面设计',
+        }
+      ],
+    },
+    freshOrNot: ["非应届生", "应届生"],
+
+    degrees: ['高中','大专','本科','硕士','博士'],
+    degreeIndex: 0,
+    years: [],
+    currentYear:'',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options.id+'4556')
-    try {
-      api.getFindById.call(this, options.id)
-    } catch (e) {
-      console.log('获取编号为' + options.id + '的文档失败')
+    //console.log(options)
+
+    /* 获取数据 */
+    this.fakeEmptyResume();
+    //api.xxx.call(this,options.uid,options.jid,...);
+ 
+    /* 初始化处理, 顺序不要颠倒, 且应该都要放在 then 函数里！！！ */
+    this.ensureInternAndEducation();   // 保证至少有一个education和internship
+    this.initialExpectation(options);   // 根据职位信息填充预期职位和预期工作地点
+    this.initialYears();  // 初始化年份数组和教育经历的年份picker选中信息
+    this.initialDegreeIndex();  // 为所有学历字段添加当前选择的索引信息
+  },
+
+  fakeEmptyResume: function(){
+    this.data.resume= {
+      name: '',
+      face: "/img/user.png",
+      isFresh: false,
+      degree: '',
+      age: 0,
+      phone: '',
+      email: '',
+      experience: 0,
+      expectPosition: '', 
+      expectCity: '', 
+      lowWage: 0,
+      highWage: 0,
+      internship:[],
+      education:[],
     }
+    this.setData(this.data);
+  },
+
+  ensureInternAndEducation: function (){
+    if(this.data.resume.internship.length == 0){
+      this.data.resume.internship.push({ company: "", fromTime: '', toTime: '', position: '', grade: '', content: '' });
+    }
+    if (this.data.resume.education.length == 0) {
+      this.data.resume.education.push({ school: "", fromTime: '', toTime: '', degree: '', major: '' });
+    }
+  },
+
+  initialExpectation: function (options){
+    //console.log(options);
+    this.data.resume.expectPosition = options.expectPosition;
+    this.data.resume.expectCity = options.city;
+    this.setData(this.data);
+  },
+
+  initialDegreeIndex: function () {
+    this.data.degreeIndex=this.data.degrees.indexOf(this.data.resume.degree);
+    for (var i = 0; i < this.data.resume.education.length;i++){
+      // 添加一个degreeIndex
+      this.data.resume.education[i].degreeIndex = this.data.degrees.indexOf(this.data.resume.education[i].degree);
+    }
+    this.setData(this.data);
+  },
+
+  initialYears: function(){
+    var date = new Date(Date.parse(new Date()));
+    // 获取当前年份  
+    var currentYear = date.getFullYear();
+    this.data.currentYear = currentYear.toString();
+    // 初始化可能的年份数组，因为picker的value是数组的下标，为了统一只能从0开始
+    this.data.years = this.getLoopArray(0, currentYear + 4); 
+    
+    // 对教育经历中的年份为空的进行预先选择当前年份
+    for(var i = 0;i<this.data.resume.education.length;i++){
+      if (this.data.years.indexOf(this.data.resume.education[i].fromTime == -1)) {
+        this.data.resume.education[i].fromTime = currentYear.toString();
+      }
+      if (this.data.years.indexOf(this.data.resume.education[i].toTime == -1)) {
+        this.data.resume.education[i].toTime = currentYear.toString();
+      }
+    }
+
+    this.setData(this.data);
+  },
+
+  getLoopArray: function (start, end) {
+    var start = start || 0;
+    var end = end || 1;
+    var array = [];
+    for(var i = start; i <= end; i++) {
+      array.push(i);
+    }
+    return array;
+  },
+
+  changeDate: function(event){
+    var i = event.currentTarget.dataset.index;
+    if (event.currentTarget.dataset.kind == 'from'){
+      //把2018-01-01 替换为 2018.01.01
+      this.data.resume.internship[i].fromTime=event.detail.value.replace(/-/g,'.');
+    }
+    else if(event.currentTarget.dataset.kind == 'to'){
+      this.data.resume.internship[i].toTime = event.detail.value.replace(/-/g, '.');
+    }
+    this.setData(this.data);
+  },
+
+  changeYear: function (event) {
+    var i = event.currentTarget.dataset.index;
+    if (event.currentTarget.dataset.kind == 'from') {
+      this.data.resume.education[i].fromTime = event.detail.value;
+    }
+    else if (event.currentTarget.dataset.kind == 'to') {
+      this.data.resume.education[i].toTime = event.detail.value;
+      //数组不从0开始则this.data.resume.education[i].toTime = this.data.years[event.detail.value]
+    }
+    this.setData(this.data);
+  },
+
+ 
+  changeFresh: function (event) {
+    this.data.resume.isFresh = event.detail.value == 1 ? true : false;
+    this.setData(this.data);
+  },
+
+  changeDegree: function (event) {
+    this.data.degreeIndex = event.detail.value;
+    this.data.resume.degree = this.data.degrees[this.data.degreeIndex];
+    this.setData(this.data);
+  },
+
+  changeItemDegree:function(event){
+    var i = event.currentTarget.dataset.index;
+    this.data.resume.education[i].degreeIndex = event.detail.value;
+    this.data.resume.education[i].degree = this.data.degrees[event.detail.value];
+    this.setData(this.data);
+  },
+
+  updateName: function (e) {
+    this.data.resume.name = e.detail.value;
+  },
+  updateAge: function (e) {
+    this.data.resume.age = e.detail.value;
+  },
+  updatePhone: function (e) {
+    this.data.resume.phone = e.detail.value;
+  },
+  updateEmail: function (e) {
+    this.data.resume.email = e.detail.value;
+  },
+  updateExperience: function (e) {
+    this.data.resume.experience = e.detail.value;
+  },
+  updateLow: function (e) {
+    this.data.resume.lowWage = e.detail.value;
+  },
+  updateHigh: function (e) {
+    this.data.resume.highWage = e.detail.value;
+  },
+  updateCompany: function (e) {
+    var i = e.currentTarget.dataset.index;
+    this.data.resume.internship[i].company = e.detail.value;
+  },
+  updatePosition: function (e) {
+    var i = e.currentTarget.dataset.index;
+    this.data.resume.internship[i].position = e.detail.value;
+  },
+  updateGrade: function (e) {
+    var i = e.currentTarget.dataset.index;
+    this.data.resume.internship[i].grade = e.detail.value;
+  },
+  updateContent: function (e) {
+    var i = e.currentTarget.dataset.index;
+    this.data.resume.internship[i].content = e.detail.value;
+  },
+  updateSchool: function (e) {
+    var i = e.currentTarget.dataset.index;
+    this.data.resume.education[i].school = e.detail.value;
+  },
+  updateMajor: function (e) {
+    var i = e.currentTarget.dataset.index;
+    this.data.resume.education[i].major = e.detail.value;
+  },
+
+  preview: function(){
+    this.data.isPreviewing = !this.data.isPreviewing;
+    this.setData(this.data);
+  },
+  
+  save: function () {
+    // 只保存不需要检查信息完整性
+    console.log(this.data.resume)
+    //api.xxx.call(this)
+  },
+
+  saveAndSend: function (){
+    if (this.checkInfomation()) {
+      console.log(this.data.resume)
+      wx.showModal({
+        title: '提示',
+        content: '是否确认投递简历？',
+        success: function (res) {
+          if (res.confirm) {
+            //api.yyy.call(this)
+          } 
+        }
+      })
+    }
+  },
+
+  checkInfomation: function (){
+    /* 检查输入合法性 */
+    if (typeof (this.data.resume.name) == undefined || this.data.resume.name==''){
+      wx.showToast({
+        title: '请填写姓名',
+        icon: 'none'
+      })
+      return false;
+    } 
+    if (typeof (this.data.resume.degree) == undefined || this.data.resume.degree == '') {
+      wx.showToast({
+        title: '请填写最高学历',
+        icon: 'none'
+      })
+      return false;
+    }
+    if (typeof (this.data.resume.phone) == undefined || this.data.resume.phone == '') {
+      wx.showToast({
+        title: '请填写手机',
+        icon: 'none'
+      })
+      return false;
+    }
+    if (typeof (this.data.resume.email) == undefined || this.data.resume.email == '') {
+      wx.showToast({
+        title: '请填写邮箱',
+        icon: 'none'
+      })
+      return false;
+    }
+    if (!/^1[34578]\d{9}$/.test(this.data.resume.phone)) {
+      wx.showToast({
+        title: '手机号码有误，请重填',
+        icon: 'none'
+      })
+      return false;
+    }
+    if (!/^\w+((.\w+)|(-\w+))@[A-Za-z0-9]+((.|-)[A-Za-z0-9]+).[A-Za-z0-9]+$/.test(this.data.resume.email)) {
+      wx.showToast({
+        title: '邮箱地址有误，请重填',
+        icon: 'none'
+      })
+      return false;
+    }
+    return true;
+  },
+
+  addInternship:function(){
+    this.data.resume.internship.push({ company: "", fromTime: '', toTime: '', position: '', grade: '', content: '' });
+    this.setData(this.data);
+  },
+
+  deleteInternship: function (e) {
+    var i = e.currentTarget.dataset.index;
+    this.data.resume.internship.splice(i,1);
+    this.setData(this.data);
+  },
+
+  addEducation: function () {
+    /* 这里要直接对fromTime ， toTime ， degreeIndex 做初始化!!! */
+    this.data.resume.education.push({ school: "", fromTime: this.data.currentYear, toTime: this.data.currentYear, degree: '', major: '' ,degreeIndex:-1});
+    this.setData(this.data);
+  },
+
+  deleteEducation: function (e) {
+    var i = e.currentTarget.dataset.index;
+    this.data.resume.education.splice(i, 1);
+    this.setData(this.data);
   },
 
   /**
